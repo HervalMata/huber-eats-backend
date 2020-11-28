97a7591e8074888b2662adb766a80823a8d5dd59
HervalMata <hervalmataamparo@gmail.com>
2020-11-27 23:05:15 -0300

    TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'math3036',
+      database: 'huber-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
     autoSchemaFile: true,
   }),],
